653d201f839ee3107c70d531748cd1bb2510eba7
KTH <KTH@218.148.205.10>
2021-10-06 09:18:34 +0900

    9:18

--- atm/src/controller/사용자매니저.java
+++ atm/src/controller/사용자매니저.java
@@ -1,9 +1,41 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import models.User;
+
 public class 사용자매니저 {
+	public static 사용자매니저 instance = new 사용자매니저();
+	Scanner sc = new Scanner(System.in);
+	private ArrayList<User> users = new ArrayList<User>();
+
+	private 사용자매니저() {
+		this.users = new ArrayList<>();
+	}
+
+	public void joinUser() {
+		System.out.println("id : ");
+		String id = BankManager.sc.next();
+		System.out.println("pw : ");
+		String pw = BankManager.sc.next();
+
+		boolean check = false;
+		for (User user : users) {
+			if (id.equals(user.getId()))
+				check = true;
+		}
+		if(!check) {
+			System.out.print("이름 입력 : ");
+			String name = BankManager.sc.next();
+			
+			this.users.add(new User(randomCode(), id, pw, name));
+		}else {
+			System.out.println("중복된 아이디 입니다.");
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
--- atm/src/models/사용자.java
+++ atm/src/models/사용자.java
@@ -1,10 +1,12 @@
 package models;
+import controller.사용자매니저;
+
 
 public class 사용자 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
 	}
 
 }
