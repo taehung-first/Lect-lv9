122f26f9eee13d4d1779891bbe25ca9ac28d9909
KTH <KTH@59.7.148.210>
2021-10-20 15:26:25 +0900

    .

---
+++ lv10test_zombie/src/models/ZombieKing.java
@@ -0,0 +1,10 @@
+package models;
+
+import control.Unit;
+
+public abstract class ZombieKing  extends Unit{
+	public ZombieKing(String name,int hp,int att,int pos,int shield) {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+}
