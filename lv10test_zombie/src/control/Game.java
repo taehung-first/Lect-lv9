49b06e7d9071fc72f2ac732e4bd5a7cde3996d96
KTH <KTH@59.7.148.210>
2021-10-20 14:12:26 +0900

    212

---
+++ lv10test_zombie/src/control/Game.java
@@ -0,0 +1,17 @@
+package control;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+	Scanner scan = new Scanner(System.in);
+	Random ran = new Random();
+	
+	private Game() {
+		
+	}
+	private ArrayList<Unit> enemy = new ArrayList<>(); 
+	private void init() {
+	}
+}
---
+++ lv10test_zombie/src/models/Hero.java
@@ -0,0 +1,11 @@
+package models;
+
+import control.Unit;
+
+public class Hero extends Unit{
+	public Hero(String name, int hp, int att, int def, int pos) {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
